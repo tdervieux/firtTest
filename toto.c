9cf6057b9027211ddc321d044ce4587582fa3198
tito155 <thierry_dervieux@hotmail.com>
2016-07-02 15:17:46 +0200

    ca c'est la suite

--- toto.c
+++ toto.c
@@ -3,3 +3,7 @@ now there is a change !
 bon on va raconter une histoire ...
 c'est l;histoire d'un cochon etc
 Je vais garder cette histoire sur git.
+
+
+et maintenant voila la suite
+Le cochon s'appelle reudodin
